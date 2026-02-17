0aff5dcb702e2dafcfe9d6e2d0ee1f369c7009d9
Arav720 <aravmk156@gmail.com>
2026-02-17 16:58:39 +0530

    refactor: remove backend event management codebase
    - Deleted package.json and all controller files related to authentication, organizers, and users.
    - Removed index.ts, db.ts, middleware, model files, and route files.
    - Cleared TypeScript configuration file.

--- event-management-backend/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import  { Router} from 'express'
-import { loginUser, registerUser } from '../controller/auth.ts';
-
-const authRoutes = Router();
-
-authRoutes.post("/register", registerUser);
-authRoutes.post("/login", loginUser);
-
-export default authRoutes
